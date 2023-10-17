8ff5cb579d696278b1181f28541511e690f4b3d9
moniquetrotta <monique.trotta@gmail.com>
2023-10-17 19:24:44 -0300

    componentes dinamicos

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
-
+  @Input()
+ imgCover:string =""
+  @Input()
+ cardTitle:string = ""
+ 
   constructor() { }
 
   ngOnInit(): void {
